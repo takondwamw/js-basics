88cd3fccc1ea459873aa05524b903920fe05369a
Takondwa Kapyola <takondwakapyola@hotmail.com>
2023-08-16 10:58:21 +0200

    made changes
    learning continued - stopped due to internet bundle

--- todo.js
+++ todo.js
@@ -1,6 +1,8 @@
 // create the do 
 let id =  Math.round(Math.random()) ;
 let task = []; 
+removeEgg(['egg','apple','egg','egg','egg','ham']);
+fisrtTwo(['egg','apple','egg','egg','egg','ham']); 
 
 
 function $(elem){
@@ -34,20 +36,25 @@ function fromStorage (obj){
 }
 // display on webpage
 function updatePage(obj){
+
         let tag =$('.task');
+
         let pended = '';
+
         task = obj === 'object' ? task = obj : task ;
+
         const result = {};
+
         for(let i=0; i < obj.length;i++){
             const taskObject = obj[i];
             const {task_name,taskDate} = taskObject;
-            const html =`<div>${ task_name }</div> <div>${taskDate}</div> <button onclick="obj.splice(${i},1)" class="btn danger">Delete</button></p>`;
+            const html =`<div>${ task_name }</div> <div>${taskDate}</div> <button onclick="${ obj.splice(i,0) }" class="btn danger">Delete</button></p>`;
             pended += html;
 
+            
         }
-        console.log(pended)
-        tag.innerHTML = pended;
-  
+        // console.log(pended)
+        tag.innerHTML = pended;  
     }
 // select element
 function $(elm){
@@ -60,8 +67,29 @@ function del(id,arr){
     console.log(arr)
 
 }
-// class TodoApp {
-//     constructor(){
-//         this.
-//     }
-// }
+
+function removeEgg(arr){
+    let newArr = arr.filter(val => {
+        return  val !== "egg";
+    });
+    return console.log( newArr );
+}
+
+function fisrtTwo (arr){
+        let count = 0;
+        
+            let m = arr.filter(val => { 
+                if(val == "egg" && count <= 2){
+                    count++;
+                    return false;
+                }else{
+                    return true;
+                }
+            
+            })
+        //    break;
+
+        // }
+        return console.log(m);
+        
+}
